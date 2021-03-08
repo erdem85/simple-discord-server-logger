a4e1237e5dd61cd5722607307bb68565a0a307d9
erdem <erdemhso@mail.ru>
2021-03-08 10:49:53 +0300

    Update setup1.js

--- setup1.js
+++ setup1.js
@@ -3,8 +3,8 @@ const config = require("./config")
 const Discord = require("discord.js")
 const client1 = new Discord.Client({user: config.firstacc.user})
 client1.login(config.firstacc.token)
-const client2 = new Discord.Client({user: config.secondacc.user})
-client2.login(cconfig.oneacc ? config.firstacc.token : config.secondacc.token)
+const client2 = new Discord.Client({user: config.oneacc ? config.firstacc.user : config.secondacc.user})
+client2.login(config.oneacc ? config.firstacc.token : config.secondacc.token)
 
 client1.on("ready", async () => {
     console.log("Starting...")
