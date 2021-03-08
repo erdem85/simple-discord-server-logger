f871de695a1bc216bb69bac7c861130d090b5ff0
erdem <erdemhso@mail.ru>
2021-03-08 10:50:11 +0300

    Update clear.js

--- clear.js
+++ clear.js
@@ -1,8 +1,8 @@
 const config = require("./config")
 
 const Discord = require("discord.js")
-const client = new Discord.Client({user: config.secondacc.user})
-client.login(config.secondacc.token)
+const client = new Discord.Client({user: config.oneacc ? config.firstacc.user : config.secondacc.user})
+client.login(config.oneacc ? config.firstacc.token : config.secondacc.token)
 
 client.on("ready", async () => {
     console.log("Starting...")
